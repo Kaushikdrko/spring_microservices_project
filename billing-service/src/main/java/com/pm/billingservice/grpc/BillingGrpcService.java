d7301f23b109b493e0cbae7c7616e860616e2004
Kaushik <kaushik13.shivakumar@gmail.com>
2026-03-05 00:12:02 -0600

    feat: implmented gRPC billing microservice

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,28 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase{
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+         StreamObserver<billing.BillingResponse> responseObserver){
+        
+        log.info("createBillingAccount request recieved {}", billingRequest.toString());
+
+        // Business logic - e.g save to database, perform calculates etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+            .setAccountId("12345")
+            .setStatus("Active")
+            .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+    } 
+}
